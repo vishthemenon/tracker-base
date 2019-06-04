968c87340e033894a66f48ea77045baf17f8cf44
Vishnu R Menon <vishthemenon@gmail.com>
2019-06-04 19:43:29 +0100

    Patch

--- Tracker.h
+++ Tracker.h
@@ -8,12 +8,14 @@ using namespace std;
 using namespace cv;
 
 class Tracker {
+  void loopedTracking(VideoCapture vid);
 protected:
   Mat cameraMatrix, distCoeffs;
 public:
   explicit Tracker(CVCalibration& cvl);
   virtual int getPose(Mat& frame, Vec3d& tVec, Vec3d& rVec) = 0;
   bool startStreamingTrack(int port = 0);
+  bool startVideoTrack(const string& fname);
 };
 
 #endif //ARUCO_TRACKING_TRACKER_H
